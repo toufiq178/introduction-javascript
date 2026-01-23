0b9e4631c49a14be0f42ce3c8422e246bb22f330
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-01-23 22:19:00 +0600

    add if-else

---
+++ if-else.js
@@ -0,0 +1,62 @@
+// const age = 60 ;
+// const price = 5000;
+
+// if ( age <=12) {
+    
+//     console.log('for free');
+    
+// }
+// else if (age <= 30){
+//     //10% discount
+//     const discount = price * 10 / 100;
+//     const payment = price - discount;
+//     console.log(payment)
+// }
+// else if (age <=45 )  {
+//     //45% discount
+//     const discount = price * 45 / 100 ;
+//     const payment = price - discount ;
+//     console.log(payment);
+    
+// }
+// else if (age <=60 )  {
+//     //70% discount
+//     const discount = price * 70 / 100 ;
+//     const payment = price - discount ;
+//     console.log(payment);
+    
+// }
+// else{
+
+//     console.log(price)
+// }
+
+
+
+const mark = 30 ;
+
+
+if (mark >=80) {
+
+    console.log('A+')
+    
+}
+else if (mark >=70) {
+    console.log('A')
+}
+else if (mark >=60) {
+    console.log('A-')
+}
+else if (mark >=50) {
+    console.log('B')
+}
+else if (mark >=40) {
+    console.log('C')
+}
+else if (mark >=30) {
+    console.log('D')
+}
+else {
+
+    console.log('fail')
+}
