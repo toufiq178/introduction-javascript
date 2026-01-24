b3a52a8648fd75cfb78fe263c4111ab5af12c3d5
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-01-24 20:23:41 +0600

    module 13 practice task done

--- if-else.js
+++ if-else.js
@@ -2,9 +2,9 @@
 // const price = 5000;
 
 // if ( age <=12) {
-    
+
 //     console.log('for free');
-    
+
 // }
 // else if (age <= 30){
 //     //10% discount
@@ -17,14 +17,14 @@
 //     const discount = price * 45 / 100 ;
 //     const payment = price - discount ;
 //     console.log(payment);
-    
+
 // }
 // else if (age <=60 )  {
 //     //70% discount
 //     const discount = price * 70 / 100 ;
 //     const payment = price - discount ;
 //     console.log(payment);
-    
+
 // }
 // else{
 
@@ -33,30 +33,252 @@
 
 
 
-const mark = 30 ;
+// const mark = 30 ;
 
 
-if (mark >=80) {
+// if (mark >=80) {
 
-    console.log('A+')
+//     console.log('A+')
+
+// }
+// else if (mark >=70) {
+//     console.log('A')
+// }
+// else if (mark >=60) {
+//     console.log('A-')
+// }
+// else if (mark >=50) {
+//     console.log('B')
+// }
+// else if (mark >=40) {
+//     console.log('C')
+// }
+// else if (mark >=30) {
+//     console.log('D')
+// }
+// else {
+
+//     console.log('fail')
+// }
+
+// const height = 6 ;
+// const weight = 60;
+
+// if (height <=4 && weight <=40)  {
+
+//     console.log('you passed')
+
+// }
+// else if (height <=5 && weight <=50)  {
+
+//     console.log('nice')
+
+// }
+// else if (height <=6 && weight <=60)  {
+
+//     console.log('very good')
+
+// }
+// else{
+//     console.log('fail');
+
+// }
+
+// const money = 1;
+
+// if (money > 400) {
+//     console.log('rich');
+
+
+
+// }
+// else {
+//     if (money > 300) {
+//         console.log('good');
+
+//     } else {
+//         if (money > 100) {
+
+//             console.log('ok');
+//         } else{ 
+
+//             if (money >=0) {
+//                 console.log('sob sesh');
+//             }
+
+
+//         }
+
+//     }
+
+
+
+
+// }
+
+
+// ternary  
+
+// condition ? something when true : something false
+
+// const money = 100 ;
+
+// money >= 300 ? console.log("rich") : console.log("poor")
+
+// let price = 500 ;
+
+// const leader = false ;
+
+
+// price = leader== true ? 0 : price + 100;
+
+
+
+
+
+
+//                      free drinks 
+//........................................................
+// const burger = 600 ;
+// const coke = 30 ;
+
+// if ( burger >= 500 ) {
+
+//     console.log ('free Coke')
+// }else{
+
+//     console.log('coke :30 tk')
+// }
+
+
+//                      BMI calculator 
+//...............................................................  
+// const weight =30;
+// const height = 178;
+
+// const BMI = weight / ((height / 100) * 2)
+
+
+// if (BMI < 18.5) {
+
+//     console.log("you are under weight");
+
+// } else if (BMI >= 18.5 && BMI <= 24.9) {
+
+//     console.log('you are normal')
+
+// } else if ( BMI <= 29.9) {
+
+//     console.log('you are overweight')
+// } else {
+
+//     console.log('you are obese')
+
+// }
+
+//                      grade calculator 
+//.................................................
+
+// const score = 99;
+
+// if (score >= 90) {
+//     console.log('A')
+// } else if (score >= 80) {
+//     console.log('B')
+// }
+// else if (score >= 70) {
+//     console.log('C')
+// }
+// else if (score >= 60) {
+//     console.log('D')
+// } else {
+//     console.log('fail')
+// }
+
+//                      nested-friend calculator 
+//.................................................
+// const myScore = 80 ;
+// const friendScore= 39;
+
+// if (myScore >=80) {
+
+//     if (friendScore >=80) {
+//         console.log(' go for a lunch')
+//     }else if (friendScore >=60) {
+//         console.log(' good luck next time')
+
+//     }else if (friendScore  >= 40) {
+//         console.log(' message unseen')
+
+//     }else if (friendScore <= 39) {
+//         console.log(' block ')
+
+//     }
     
-}
-else if (mark >=70) {
-    console.log('A')
-}
-else if (mark >=60) {
-    console.log('A-')
-}
-else if (mark >=50) {
-    console.log('B')
-}
-else if (mark >=40) {
-    console.log('C')
-}
-else if (mark >=30) {
-    console.log('D')
-}
-else {
+// }else{
+//     console.log(' go to home and sleep and act sad')
+// }
 
-    console.log('fail')
+//  /          ternary number 
+//.................................................
+// const num1 = 10;
+// const num2 = 30;
+// let result = '';
+
+// if (num1 > num2) {
+    
+//     result = num1 *2 ;
+//     console.log(result)
+// } else{
+
+//     result = num1 + num2 ;
+//     console.log(result)
+// }
+
+//           ternary
+
+// const num1 = 50;
+// const num2 = 30;
+// let result = '';
+// num1 > num2 ? result = num1 *2 : result = num1 + num2 ;
+// console.log(result)
+
+//               bus fare calculator 
+//...........................................
+
+let ticket = 800 ;
+const age = 40 ;
+const children = true ;
+const student = false ;
+const seniorCitizens = true ;
+
+if (children=== true && age < 10) {
+    
+    console.log('free');
+    
+}else if (student=== true){
+
+    discount= 50 / 100 *ticket
+    discount50 = ticket - discount
+    console.log(discount50)  
+
+}else if (seniorCitizens=== true && age>=60) {
+
+    discount= 15 / 100 *ticket
+    discount15 = ticket - discount
+    console.log(discount15)  
+}else{
+
+    console.log(ticket)
 }
+
+
+
+
+
+
+
+
+
+
+
