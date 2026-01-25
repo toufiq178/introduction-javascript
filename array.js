f00249a466fdeb8b2e5eae896e31de6c32999c39
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-01-25 20:36:52 +0600

    module 14.5 task

--- array.js
+++ array.js
@@ -1,5 +1,59 @@
-const numbers = [12, 33, 44, 55, 44]
-const names = ['rofik', 'sofik', 'jobbar']
-const passed = [true, false, true, false]
+// const numbers = [12, 33, 44, 55, 44]
+// const names = ['rofik', 'sofik', 'jobbar']
+// const passed = [true, false, true, false]
 
-console.log(passed.length);
+// console.log(passed.length);
+
+//task 1
+// const fruits = ['apple', 'orange', 'peyara', 'anaros', 'boroi'];
+
+// fruits[2]='jambura'
+// console.log(fruits[2]);
+// console.log(fruits.length);
+
+
+// task 2
+
+// const destination = ['shajek', 'bandorbon', 'kuakata'];
+
+// destination.push('coxBazar');
+// destination.push('lalBag', 'sainMartin');
+// destination.pop();
+// console.log(destination);
+
+
+//task 3
+
+// const books = ['javaScript', 'python','css','html', 'c++', 'c#']
+
+// if (books.includes('javaScript')) {
+    
+//     console.log('true')
+// }else{
+//     console.log('false')
+// }
+
+// task 4
+
+// const books = ['javaScript', 'python','css','html', 'c++', 'c#'];
+// const place =  ("sajek", "kuakata", "coxBazar");
+// const color = [];
+
+
+// console.log(Array.isArray(color));
+
+
+// //  join 
+// let place =  ["sajek", "kuakata", "coxBazar"];
+
+
+// let array = place.join('$');
+// console.log(array)
+
+
+// concat array 
+
+const place =  ["sajek", "kuakata", "coxBazar"];
+const books = ['javaScript', 'python','css'];
+
+console.log(place.concat(books));
