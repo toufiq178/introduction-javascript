ed986444a4811524c0e8e684e8e061a6052f6e00
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-01-25 23:41:35 +0600

    task compelete

---
+++ aiTask.js
@@ -0,0 +1,105 @@
+//task 1
+
+// const friends = ['rofik', 'sofik', 'jobbar', 'babbar'];
+// console.log(friends[3]);
+// friends[2]='kalu';
+// console.log(friends);
+
+
+// task 2
+
+// const brands = ['nokia', 'samsung', 'iphone', 'oppo', 'redmi', 'onePlus'];
+// brands.push ('infinix', 'samphony');
+// brands.pop();
+// console.log(brands);
+// console.log(brands.length);
+
+
+// //task 3
+
+// const food = ['biriyani', 'kacchi', 'pulaw'];
+// food.push("morog-polaw", 'sorshe elish');
+// food.pop()
+// console.log(food);
+
+
+// // //task 4
+
+// const city = ['dhaka', 'rajshahi', 'khulna'];
+// city.unshift ('sylet', 'chottogram')
+// city.shift ();
+// console.log(city)
+
+
+// //     task 5
+
+// const games = ['ff', 'pubg', 'car', 'bike', 'track'];
+// if (games.includes('cricket')) {
+    //     console.log('true');
+    
+    // }else{
+        //     console.log('false');
+        
+    // }
+        
+        
+        
+// // //     task 6
+
+// const array = ['a', 'b', 'c', 'd'];
+// const number = (1 , 2, 3, 4);
+// const string =('we', 'ewe', 'ewe', 'wew');
+// const empty = [];
+
+// console.log(Array.isArray(number));
+
+
+
+//      task 7
+
+// const morning = ['uthbo', 'brash krbo', 'nasta krbo ', 'scl e jbo'];
+// const evening = ['school theke asbo', 'khabo','khelte jbo'];
+
+// console.log(morning.concat(evening));
+
+//  slice task 
+
+let fruit = ['apple', 'banana', 'orange', 'mango', 'guava']
+
+fruit.splice(1,3 ,'papaya');
+
+
+console.log(fruit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
