6554792dc87022c9f5d60b0bc25e3d9ac142483b
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-01-26 22:46:48 +0600

    add loop

---
+++ loop.js
@@ -0,0 +1,167 @@
+
+// ________for of ______________loop
+
+
+// const fruits = ['apple', 'orange', 'mango', 'guava', 'starFruit','mango', 'guava','mango', 'guava']
+
+
+// for(const fruit of fruits){
+//     console.log(fruit);
+//     console.log('i want to eat');
+    
+// }
+
+
+
+
+
+// ________while______________loop
+
+
+// let num = 0;
+// while (num < 5) {
+//     console.log('lopping', num)
+//     num = num + 1;
+// }
+
+// let num =75 ;
+// while (num <=125) {
+//     console.log(num);
+//     num ++;
+    
+// }
+
+// let num = 0 ;
+// let sum = 0 ;
+
+// while (num <=10){
+
+//     console.log(num);
+//     sum = sum + num ;
+//     num ++;
+
+//     console.log("sum",sum);
+
+// }
+ 
+
+
+
+//  programming hero task
+
+// let days = 1 ;
+
+// while (days <=60) {
+    
+//     console.log(days ,"I will invest at least 6 hrs every single day for next 60 days!");
+//     days ++ ;
+
+    
+// }
+
+
+// let num = 61 ;
+
+// while (num <=100) {
+
+   
+//     if (num % 2 === 1) {
+//         console.log ('odd',num);
+//     }
+//      num ++;
+// }
+
+
+// let number = 78 ;
+// while (number <=98) {
+    
+//     number ++;
+//     if (number % 2 === 0) {
+//         console.log ('even',number);
+//     }
+    
+// }
+
+
+// sum odd num
+
+// let num = 81 ;
+// let sum = 0;
+
+// while (num <=131) {
+
+   
+//     if (num % 2 === 1) {
+//         // console.log ('odd',num);
+//         sum = num + sum;
+//         console.log ('sum',sum);
+//     }
+//      num ++;
+// }
+
+
+// sum even num
+
+// let num = 206 ;
+// let sum = 0;
+
+// while (num <=311) {
+
+   
+//     if (num % 2 === 0) {
+//         // console.log ('odd',num);
+//         sum = num + sum;
+//         console.log ('sum',sum);
+//     }
+//      num ++;
+// }
+
+
+// |____________for loop _________
+
+// for (let i = 0; i <10 ; i ++ ) {
+//     console.log(i);
+// }
+
+// odd num
+// for ( let i = 1 ; i <=20 ; i= i+2){
+//     console.log('odd', i);
+    
+// }
+
+// for (let even = 0 ; even <=20 ; even=even+2){
+//     console.log('even',even);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
