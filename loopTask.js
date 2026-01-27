898410921de0719efb75bbefcbb102c1fe3d76ef
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-01-27 16:14:50 +0600

    module 15 task compelete

--- loopTask.js
+++ loopTask.js
@@ -37,3 +37,114 @@
 //     }
 //     console.log(square);
 // }
+
+// ________________continue_____________________
+
+// for (let j = 0; j <=40; j++) {
+    
+//     if(j % 2 == 1){
+//         continue;
+//     }
+//     console.log(j);
+    
+// }
+
+
+
+// for (let k = 55; k <=85; k++) {
+    
+//     if (k % 5 === 0) {
+//         continue;
+//     }
+//     console.log(k);
+    
+// }
+
+
+// ______________________for ___________________________
+
+// for (let i = 1; i <=60; i++) {
+//     console.log(i,'I will invest at least 6 hrs every single day for next 60 days!');
+    
+    
+// }
+
+
+// for (let j = 61; j <=100; j++) {
+    
+//     if (j % 2 === 1) {
+        
+//         console.log(j,'odd')
+//     }
+    
+// }
+
+// for (let i = 78; i <=98; i++) {
+//    if (i % 2 === 0) {
+//     console.log(i, 'even');
+    
+//    }
+    
+// }
+
+
+// let multiply = 9 ;
+// let total = 0;
+// for (let i = 1; i <=10; i++) {
+    
+//     total = multiply * i ;
+
+//     console.log(multiply , 'x', i, '=',total);
+    
+// }
+
+// for (let i = 81; i >=65; i--) {
+//     console.log(i);
+    
+// }
+
+// let i = 1 ;
+// while (i <=60) {
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!", i);
+//     i ++ ;
+    
+// }
+
+
+// let i = 61 ;
+// while (i <=100) {
+//     if (i % 2 ===1) {
+//         console.log('odd', i);
+//     }
+//     i++;
+// }
+
+// let n = 78 ;
+// while (n <= 98) {
+
+//     if (n % 2 ===0) {
+//         console.log('even', n);
+
+//     }
+//     n++;
+// }
+
+
+// let i = 1 ;
+// let multiply = 5 ;
+// let total = 0;
+
+// while (i <=10) {
+    
+//     total = multiply * i ;
+//     console.log(multiply , 'x', i , '=', total);
+//     i++;
+    
+// }
+
+let i = 21 ;
+
+while (i >= 15) {
+    console.log(i);
+    i--;
+}
