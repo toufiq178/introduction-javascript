1582f6fd06069e6a3c95b78865442f361d49dffe
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-01-27 22:17:50 +0600

    commit

--- string.js
+++ string.js
@@ -39,14 +39,38 @@
 // }
 
 
-const drink = 'water';
-const liquid = '   water ';
+// const drink = 'water';
+// const liquid = '   water ';
 
-if (drink.trim() === liquid.trim()) {
+// if (drink.trim() === liquid.trim()) {
     
-    console.log('pani');
+//     console.log('pani');
     
-}else{
-    console.log('khawa jbena');
+// }else{
+//     console.log('khawa jbena');
     
-}
+// }
+
+
+// -----------------slice --------------
+
+// const address = 'bangladesh';
+// const part = address.slice(3,6);
+// console.log(part);
+
+
+//  split
+
+const sentence = 'bangladesh is a beautiful country';
+// console.log(sentence.split(' '));
+
+
+const football = ['all','time','greast','player','in','the','world','is','cr7'];
+// console.log(football.join('-'));
+
+const first = 'abid';
+const last = 'nabid';
+
+const fullName = first + ' ' + last ;
+// console.log(first.concat(" ").concat(last));
+// console.log(fullName.includes('b'))
