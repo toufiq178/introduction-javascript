dcc0dca4989f3ed30073aac7154e5b643d8d88b5
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-01-28 20:57:41 +0600

    array2

---
+++ array2.js
@@ -0,0 +1,35 @@
+// const friends = ['elon', 'bill','mark','waren' ];
+
+// for ( const friend of friends ){
+
+//     // console.log(friend);
+    
+// }
+
+// for (let i = 0;  i<friends.length ; i++) {
+    
+//     frnd = friends[i];
+//     // console.log(frnd);
+    
+// }
+
+// const numbers = [212,21,33,13,31,424,53,312,133,2321];
+
+// let i = 0 ;
+// while (i <numbers.length) {
+    
+//     num = numbers[i];
+//     console.log(num);
+//     i ++ ;
+    
+// }
+
+
+
+
+// ____________________REVERSE ARRAY ______________
+
+const numbers = [1, 3 ,4 ,5, 5, 5, 6, 6 ,7,8,9, 9];
+
+const reversed = numbers.reverse();
+console.log(reversed);
