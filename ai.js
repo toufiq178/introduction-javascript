f541d007540b4bf715c45f7eae29036d21ae1db6
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-01-30 02:16:17 +0600

    ai task7

--- ai.js
+++ ai.js
@@ -60,16 +60,77 @@
 // }
 // console.log(num);
 
-const webDeveloper = ['html', 'css', 'js'];
+// const webDeveloper = ['html', 'css', 'js'];
 
-let web = []
+// let web = []
 
-for (let i = 0; i < webDeveloper.length; i++) {
+// for (let i = 0; i < webDeveloper.length; i++) {
     
-    let w = webDeveloper[i];
-    console.log(w.toUpperCase());
-    let y = w.push(web)
+//     let w = webDeveloper[i];
+//     // console.log(w.toUpperCase());
+//     // web = web + w.toUpperCase();
+//     web.push(w.toUpperCase());
     
     
-    console.log(y);
+// }
+// console.log(web);
+
+// const number = [10, 20, 30, 40] ;
+
+// let num = false ;
+
+// for ( let i of number){
+
+//     if (i === 30) {
+        
+//         num = true ;
+//         break ;
+//     }
+// }
+// console.log(num);
+
+
+
+
+// const numbers = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6]
+// ];
+
+
+// let sum = 0
+// for ( num of numbers){
+    
+    
+//     // console.log(num);
+    
+//     for (n of num ){
+
+//         // console.log(n);
+        
+//         sum = sum + n
+//         // sum = n + n ;
+//         // n = sum;
+//     }
+// }
+// console.log(sum);
+
+const javaScript = "I love JavaScript";
+let javaS = javaScript.split(' ');
+
+let js = '';
+
+for (let i = 0; i < javaS.length; i++) {
+    
+    let j = javaS[i];
+    
+    let script = j.split('').reverse().join('');
+    js = js + script + ' '
+    
+
+    
+    
 }
+console.log('"', js ,'"');
+// console.log(js);
