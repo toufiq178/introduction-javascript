60d13fca34c74ebf56b3d0c6ded1dfff628ce85d
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-01-29 15:50:10 +0600

    array2 task more task added

--- array2.js
+++ array2.js
@@ -29,7 +29,94 @@
 
 // ____________________REVERSE ARRAY ______________
 
-const numbers = [1, 3 ,4 ,5, 5, 5, 6, 6 ,7,8,9, 9];
+// const numbers = [1, 3 ,4 ,5, 5, 5, 6, 6 ,7,8,9, 9];
 
-const reversed = numbers.reverse();
-console.log(reversed);
+// const reversed = numbers.reverse();
+// console.log(reversed);
+
+
+
+
+
+
+// const friends = ['saif', 'ovi', 'tasdid', 'jibon'];
+
+// let friend = 0 ;
+// let frnd = '';
+// while (friend <friends.length) {
+    
+//     // console.log(friend);
+//     frnd = frnd  + friends[friend]+ ' ' ;
+//     friend++;
+// }
+// console.log(frnd);
+
+
+
+// for ( const friend of friends){
+
+//     console.log(friend);
+    
+// }
+
+// let friend = ''
+// for (let i = 0; i < friends.length; i++) {
+    
+//     friend = friend + friends[i] + ' '
+    
+    
+// }
+// console.log(friend);
+
+// const numbers = [112,33,331,311,42,42];
+
+// let number = '';
+// for (let i = 0; i < numbers.length; i++) {
+    
+//     number = number+ numbers[i] + ' ';
+
+    
+// }
+// console.log(number);
+
+
+
+let numbers = [9 ,7, 0, 4, 2, 1, 9];
+
+console.log(numbers.sort());
+
+
+// let num = numbers.reverse();
+
+// console.log(num);
+
+// let reversed= '';
+// // let rev = '';
+// for (let i = 0; i < numbers.length; i++) {
+
+    
+//     reversed = numbers[i] + reversed;
+//     // rev = reversed + rev
+    
+    
+// }
+// console.log(reversed);
+
+// let rev_num = []
+// for(let num of numbers){
+
+//     // console.log(num);
+//     rev_num.unshift(num);
+
+    
+// }
+// console.log(rev_num);
+
+// let rev = [];
+
+// for (let i = numbers.length -1; i >= 0; i--) {
+
+//     console.log(numbers[i]);
+    
+    
+// }
