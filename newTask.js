9e3f02650f239b4bfff20f9f7c57f63cf22c9a03
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-01-30 17:32:34 +0600

    ne ai taskadd

--- newTask.js
+++ newTask.js
@@ -41,22 +41,90 @@
 
 
 // Loop through an object and print the key-value pairs with their types
-let myObject = {
+// let myObject = {
 
-    name: 'John Doe',
-    age: 25,
-    city: 'Example City',
-    isStudent: true
-};
+//     name: 'John Doe',
+//     age: 25,
+//     city: 'Example City',
+//     isStudent: true
+// };
 
-for ( let object in myObject){
+// for ( let object in myObject){
 
 
-    let key = object ;
-    let value = myObject[object];
+//     let key = object ;
+//     let value = myObject[object];
 
-    console.log('key :' , key ,'|' , 'type : ', typeof value);
+//     console.log('key :' , key ,'|' , 'type : ', typeof value);
     
-}
+// }
 
 
+// Write a JavaScript code to get only the odd numbers from an array and store them in a new array.
+// Expected Output: [15, 25, 35]
+
+// const numbers = [10, 15, 20, 25, 30, 35];
+
+// // let num = ''
+// let n = []
+// for (let i = 0; i < numbers.length; i++) {
+
+//     const element = numbers[i];
+
+//     if (element % 2 === 1) {
+        
+//         n.push(element)
+        
+//     }
+    
+// }
+// console.log(n);
+
+// Use a loop to calculate the total sum of all numbers in an array.
+// Input:
+// Expected Output: 1000
+
+//  const prices = [100, 200, 300, 400];
+
+//  let p = 0
+//  for (let i = 0; i < prices.length; i++) {
+
+//     const element = prices[i];    
+//     p = element + p
+// }
+// console.log(p);
+// //  console.log(p);
+ 
+// Find the Longest Word
+// Given an array of strings, find the word with the most characters and print it.
+// Expected Output: 'Programming' (or 'Development')
+// Input: 
+const names = ['Programming', 'Hero', 'Web', 'Demvelopment'];
+
+let l = names[0]
+
+for ( let i of names){
+
+    if (i.length > l.length) {
+        
+        l = i
+    }
+}console.log(l);
+
+
+
+// let n = names[0]
+
+// for (let i = 0; i < names.length; i++) {
+
+//     const element = names[i];
+//     // console.log(element);
+    
+//    if (element.length > n.length) {
+    
+//         n = element
+    
+//    }
+
+    
+// }console.log(n);
