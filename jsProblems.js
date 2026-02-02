4bbcec85c3271811212d69758636d7fcafb54825
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-02-02 13:00:31 +0600

    module 18 class

--- jsProblems.js
+++ jsProblems.js
@@ -89,7 +89,7 @@
 // console.log(eAvg);
 
 
-// function removeDuplicate(arr) {
+// function removeDuplicate(arr ) {
     
 //     let numbr = [];
 //     for( const num of arr ){
@@ -103,7 +103,7 @@
 // }
 
 // const numbers = [ 1, 3 , 4, 4, 2 , 4, 6, 7 ,4, 4,7, 3 ,2,];
-// const number = removeDuplicate(numbers);
+// const number = removeDuplicate(numbers , 5);
 // console.log(number);
 
 
@@ -124,3 +124,67 @@
 // const nm = removeDuplicate(names);
 // console.log(nm);
 
+// const max = Math.min (2 ,3 ,3 ,4,7 ,40)
+// console.log(max);
+// const abs = Math.abs (2 - 40)
+// console.log(abs);
+// const randm = Math.round(3.7)
+// console.log(randm);
+// const flr = Math.floor(3.1)
+// console.log(flr);
+// const celi = Math.ceil(3.1)
+// console.log(celi);
+// const rn = Math.random(3.1)*10
+// console.log(rn);
+// const p = Math.pow(2,3)
+// console.log(p);
+// const p = Math.sqrt(8)
+// console.log(p);
+
+// for (var i = 0; i < 5; i++) {
+//  	 setTimeout(function () {
+//  	console.log(i);
+//   }, 100);
+// }
+
+// let a = 'stttt';
+// let b = 9 ;
+
+// let temp = a ;
+
+// a = b ;
+// b = temp ;
+
+// console.log(a , b);
+
+
+// function name(params , para1) {
+    
+
+// }
+
+// const today = new Date('');
+// const date = new Date('2050-10-26');
+// console.log(date.getMonth());
+// console.log(date.getDay());
+// const sdate = new Date(2050,1,  26)
+// sdate.setMonth(10);
+// console.log(sdate.toLocaleString('en-GB'));
+
+
+// 
+
+let a = 5 ;
+let b = 9 ;
+
+let temp = a ;
+
+a = b ;
+b = temp ;
+console.log(a , b);
+
+let x = 8 ;
+let y = 9 ;
+
+[x,y] = [y,x];
+console.log(x,y);
