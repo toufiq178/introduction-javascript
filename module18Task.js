4a054d99339da5781e6cdcaf34c6eca3ed169139
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-02-02 18:26:49 +0600

    module 19 class

---
+++ module18Task.js
@@ -0,0 +1,76 @@
+// Write a function to convert temperature from Celsius to Fahrenheit. 33.8
+
+// function celsiusToFahrenheit(celsius) {
+    
+//     let fahrenheit = (celsius * 9 / 5 ) + 32;
+//     return fahrenheit ;
+// }
+
+// const clc = 7 ;
+// const fahrenheit = celsiusToFahrenheit(clc);
+// console.log(fahrenheit);
+
+
+// You are given an array of numbers. Count how many times the a number is repeated in the array.
+
+// const numbers = [5,6,11,12,5,98, 5] ;
+
+// let count = 0 ;
+// for( let number of numbers){
+//     if (number === 25) {
+//         count++
+//     }
+
+// }
+// console.log(count);
+
+
+
+
+
+// Write a function to count the number of vowels in a string.
+// function countVowels(str) {
+    
+    
+//     const vowels = ['a','e','i','o','u'];
+//     let count = 0 ;
+//     for ( let vowel of str.toLowerCase()){
+
+//         if (vowels.includes(vowel)) {
+            
+//             count ++;
+//         }
+//     }
+//     return count ;
+// }
+
+
+// const string = 'bangladesh';
+// const result = countVowels(string);
+// console.log(result);
+
+
+// function longestWord(str) {
+    
+//     const spt = str.split(' ');
+
+//     let longWord = ''
+    
+//     for(let word of spt){
+
+//         if (word.length > longWord.length ) {
+            
+//             longWord = word ;
+//         }
+//     }
+//     return longWord ;
+// }
+
+// const string = 'I am learning Programming to become a programmer';
+// const result = longestWord(string);
+// console.log(result);
+
+
+// const number = Math.floor (Math.random() *10)+ 10
+// console.log(number);
+
---
+++ module19.js
@@ -0,0 +1,42 @@
+
+// const jim = 149 ;
+// const kim = 787 ;
+// const rim = 324 ;
+
+
+// if (jim > kim && jim > rim) {
+    
+//     console.log('jim win');
+    
+// }else if (kim > jim && kim > rim) {
+//     console.log('kim win');
+    
+// }else if (rim > jim && rim > kim) {
+//    console.log('rim win');
+   
+// }
+
+// function maxOffThree(p1 , p2 , p3) {
+    
+//     if (p1 > p2 && p1 > p3) {
+        
+//         return  console.log('win1');
+        
+//     } else if (p2 > p1 && p2 >p3) {
+//          return console.log('win2');
+        
+//     }else if (p3 > p1 && p3 > p2) {
+//          return console.log('win3');
+        
+//     }
+   
+// }
+
+// const n1 = 344;
+// const n2 = 673;
+// const n3 = 423;
+
+// const result = maxOffThree(n1 , n2 ,n3);
+// // console.log(result);
+// const max = Math.max(45, 354,63,63,343,622,5353,633343,5333333);
+// console.log(max);
