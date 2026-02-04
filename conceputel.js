7b6787ea6787e1c931c71e67e978c6afa88a4691
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-02-04 20:15:58 +0600

    conceputel task

--- conceputel.js
+++ conceputel.js
@@ -104,51 +104,214 @@
 // Output: { valid: true, reasons: [] }
 
 
-function checkPassword(password) {
+// function checkPassword(password) {
 
-    let reasons = [];
-    let length = password.length ;
-    // console.log(password);
-    let hasNumber = false ;
-    let hsUpperCase = false ;
-    let hasSpace = password.includes(' ');
+//     let reasons = [];
+//     let length = password.length ;
+//     // console.log(password);
+//     let hasNumber = false ;
+//     let hsUpperCase = false ;
+//     let hasSpace = password.includes(' ');
 
-    for (let character of password){
-        // console.log(character);
+//     for (let character of password){
+//         // console.log(character);
         
-        if (character >= '0' && character <= '9' ) {
-            hasNumber = true ;
-        }
+//         if (character >= '0' && character <= '9' ) {
+//             hasNumber = true ;
+//         }
 
-        if (character >= 'A' && character <= 'Z') {
-            hsUpperCase = true ;
+//         if (character >= 'A' && character <= 'Z') {
+//             hsUpperCase = true ;
+//         }
+//     }
+    
+//     if (length < 8 ){
+//         reasons.push('Length must be at least 8 ');
+//     }
+//     if (hasNumber == false) {
+//         reasons.push(' Must contain at least 1 number');
+//     }
+//     if (hsUpperCase == false) {
+//         reasons.push('Must contain at least 1 uppercase letter');
+//     }
+//     if (hasSpace == true) {
+//         reasons.push('Must not contain spaces');
+//     }
+  
+//     let isValid = false;
+//     if (reasons.length == 0) {
+//         isValid = true
+//     }else{
+//         isValid = false ;
+//     }
+//     return{
+//         valid : isValid, 
+//         reasons
+//     }
+// }
+
+// let result = checkPassword('heYUGUYG 2');
+// console.log(result);
+
+
+
+
+
+
+
+// Problem 4: Shopping Bill Calculator
+// Function name: calcBill(prices, items)
+// Statement:  Calculate total bill amount and count how many times each item appears.
+// Test case 1
+// Input:
+//      prices = { rice: 70, oil: 180, egg: 12, sugar: 90 };
+//      items = ["egg", "egg", "rice", "oil", "egg", "sugar"];
+
+// Output:
+// {
+//   total: 376,
+//   itemCount: { egg: 3, rice: 1, oil: 1, sugar: 1 }
+// }
+
+// Test case 2
+// Input:
+// prices = { pen: 10, book: 50 };
+// items = ["pen", "pen", "book", "pen"];
+
+// Output:
+// {
+//   total: 80,
+//   itemCount: { pen: 3, book: 1 }
+// }
+
+
+
+// function calcBill(prices, items) {
+
+//     let itemCount = {} ;
+
+//     for( let item of items){
+
+//         if (itemCount.hasOwnProperty(item)) {
+            
+//             itemCount[item] ++ ;
+//         }else{
+
+//             itemCount[item] = 1 ;
+//         }
+
+//     }
+
+//     // console.log(itemCount);
+//     let total = 0 ;
+
+//     for(let item in itemCount){
+
+//         let price = prices[item];
+//         let itemQuantity = itemCount[item];
+//         total = total + price * itemQuantity;     
+//     }
+    
+//     return {
+//         total,
+//         itemCount
+//     }
+    
+// }
+
+// let output = calcBill({ pen: 10, book: 50 }, ["pen", "pen", "book", "pen"]);
+// console.log(output);
+
+
+// function removeDuplicate(numbers) {
+    
+//     let freshArr = [];
+//     for(let num of numbers){
+
+//         if (freshArr.includes(num)=== false) {
+            
+//             freshArr.push(num);
+//         }
+//     }
+//     return freshArr ;
+// }
+
+// const arr = [1,1,1,1,1];
+// const freshArr = removeDuplicate(arr);
+// console.log(freshArr);
+
+
+
+// function searchByPrefix(phoneBook, prefix) {
+    
+//     let names = [];
+//     for ( let nm in phoneBook){
+//         console.log(nm);
+        
+//         if(nm.startsWith(prefix)){
+//             names.push(nm);
+//         }
+//     }
+//     return names ;
+// }
+
+// const phoneBook = {
+//     rahim: '0181' , 
+//     karim: '0172',
+//     rfi : '0193',
+// }
+// const prefix =  'r';
+// const output =searchByPrefix(phoneBook , prefix);
+// console.log(output);
+
+
+
+function testStates(text) {
+    
+    let character = text.split('');
+    // console.log(character);
+    let freshCharacter = [];
+    
+    for( let freshChtr of character){
+        
+        if (freshChtr !== ' ') {
+            
+            freshCharacter.push(freshChtr);
         }
+    }
+    // console.log(freshCharacter);
+    
+    
+    let vowelsArr = ['a' , 'e', 'i', 'o', 'u'];
+    let vowels = 0;
+    let consonant = 0 ;
+    let characters = freshCharacter.length;
+    let word = text.split(' ');
+    let words = word.length ;
+
+
+    for (let chtr of freshCharacter){
+
+        let lowerCaseChtr = chtr.toLowerCase();
+        // console.log(lowerCaseChtr);
+        
+        if (vowelsArr.includes(lowerCaseChtr) === true) {
+            
+            vowels ++ ;
+        } else{
+            consonant ++ ;
+        }
+    }
+    console.log(vowels , consonant , characters , words);
+
+    return {
+        characters ,
+        words ,
+        vowels ,
+        consonant,
     }
     
-    if (length < 8 ){
-        reasons.push('Length must be at least 8 ');
-    }
-    if (hasNumber == false) {
-        reasons.push(' Must contain at least 1 number');
-    }
-    if (hsUpperCase == false) {
-        reasons.push('Must contain at least 1 uppercase letter');
-    }
-    if (hasSpace == true) {
-        reasons.push('Must not contain spaces');
-    }
-  
-    let isValid = false;
-    if (reasons.length == 0) {
-        isValid = true
-    }else{
-        isValid = false ;
-    }
-    return{
-        valid : isValid, 
-        reasons
-    }
 }
 
-let result = checkPassword('heYUGUYG 2');
-console.log(result);
+const input = testStates('JavaScript is fun to learn');
+console.log(input);
