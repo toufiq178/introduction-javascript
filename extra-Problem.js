64fc7cd9fc4e25987faa855d938c17c56f6b566a
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-02-08 17:57:45 +0600

    extra task gpt2

--- extra-Problem.js
+++ extra-Problem.js
@@ -1,95 +1,153 @@
-// function finalScore(obj) {
+// // function finalScore(obj) {
     
-//     const questions = obj.correct + obj.incorrect + obj.blank;
+// //     const questions = obj.correct + obj.incorrect + obj.blank;
 
-//     if (questions !== 80 || typeof questions !== 'number') {
-//         return 'invalid';
-//     }
+// //     if (questions !== 80 || typeof questions !== 'number') {
+// //         return 'invalid';
+// //     }
 
-//     const correctMark = obj.correct * 1.25 ;
-//     const incorrectMark = obj.incorrect * 0.75 ;
-//     const skipMark = obj.blank * 0 ;
+// //     const correctMark = obj.correct * 1.25 ;
+// //     const incorrectMark = obj.incorrect * 0.75 ;
+// //     const skipMark = obj.blank * 0 ;
 
-//     const finalScore = (correctMark - incorrectMark ) ;
-//     return Math.round(finalScore) ;
+// //     const finalScore = (correctMark - incorrectMark ) ;
+// //     return Math.round(finalScore) ;
 
     
+// // }
+
+// // const obj= { correct: 50, incorrect: 20, blank: 10 }
+
+// // const score = finalScore(obj);
+// // console.log(score);
+
+// // problem 1
+// function electricBill(unit, rate) {
+//    // your code here
+
+//    if (typeof unit !== 'number' || typeof rate !== 'number' || unit <= 0 || rate <=0) {
+//         return 'Invalid';
+//    }
+
+//    const totalBill = (unit * rate ).toFixed(2);
+//    return totalBill ;
 // }
 
-// const obj= { correct: 50, incorrect: 20, blank: 10 }
+// const result = electricBill(50 , 2);
+// console.log(result);
 
-// const score = finalScore(obj);
-// console.log(score);
+
+// // problem 2
+// function validUsername(username) {
+//    // your code here
+
+   
+   
+//    if (typeof username !== 'string') {
+//        return 'Invalid'
+//     }
+    
+//     const firstCharacter = username[0];
+    
+//     if (username.length < 5 || username.length > 15) {
+//         return false ;
+
+//     } else if (username.includes(' ') || username === '') {
+//         return false ;
+
+//     }else if (!((firstCharacter >= 'a' && firstCharacter <= 'z') ||(firstCharacter >= 'A' && firstCharacter <= 'Z') ) ) {
+//         return false;
+//     }else{
+//         return true ;
+//     }
+
+// }
+
+// const username = validUsername("ArSa1j");
+// console.log(username);
+
+
+// // PROBLEM 3
+// function loanEligible(info) {
+//    // your code here
+
+//    if (typeof info !== 'object') {
+//     return 'Invalid';
+
+//    }
+
+//    if ( !( info.hasOwnProperty('age') &&  info.hasOwnProperty('income')  && info.hasOwnProperty('hasJob')  )) {
+//         return 'invalid';
+//    }
+
+//     if (info.age < 21 || info.income <20000 || info.hasJob === false ) {
+        
+//         return false ;
+//     } else {
+//         return true ;
+//     }
+
+// }
+
+
+// const obj =loanEligible({ age: 25, income: 30000 }) ;
+// console.log(obj);
+
+
 
 // problem 1
-function electricBill(unit, rate) {
-   // your code here
 
-   if (typeof unit !== 'number' || typeof rate !== 'number' || unit <= 0 || rate <=0) {
-        return 'Invalid';
-   }
+// function groceryDiscount(price, discount) {
+//    // your code here
 
-   const totalBill = (unit * rate ).toFixed(2);
-   return totalBill ;
-}
+//    if ( !(typeof price ==='number' && typeof discount ==='number')) {
+//         return 'Invalid';
 
-const result = electricBill(50 , 2);
-console.log(result);
+//     }else if (price < 0  ||  discount < 0 || discount >= 100) {
+        
+//         return 'Invalid';
+//     }
+
+//     const newPrice = price - (price * discount / 100 ).toFixed(2);
+//     return newPrice ;
+
+// }
+
+// const finalPrice = groceryDiscount(1000, 100);
+// console.log(finalPrice);
+
 
 
 // problem 2
-function validUsername(username) {
+function validMobile(mobile) {
    // your code here
 
-   
-   
-   if (typeof username !== 'string') {
-       return 'Invalid'
-    }
-    
-    const firstCharacter = username[0];
-    
-    if (username.length < 5 || username.length > 15) {
-        return false ;
+   if (typeof mobile !== 'string') {
+        return 'Invalid';
+   }
 
-    } else if (username.includes(' ') || username === '') {
-        return false ;
-
-    }else if (!((firstCharacter >= 'a' && firstCharacter <= 'z') ||(firstCharacter >= 'A' && firstCharacter <= 'Z') ) ) {
+   if ( mobile.length !== 11) {
         return false;
-    }else{
-        return true ;
-    }
 
-}
-
-const username = validUsername("ArSa1j");
-console.log(username);
-
-
-// PROBLEM 3
-function loanEligible(info) {
-   // your code here
-
-   if (typeof info !== 'object') {
-    return 'Invalid';
-
-   }
-
-   if ( !( info.hasOwnProperty('age') &&  info.hasOwnProperty('income')  && info.hasOwnProperty('hasJob')  )) {
-        return 'invalid';
-   }
-
-    if (info.age < 21 || info.income <20000 || info.hasJob === false ) {
-        
+   }else if (mobile.startsWith('01') !== true) {
         return false ;
-    } else {
+   }
+
+   const num = mobile.split('');
+
+   for(let n of num){
+    if (! (n.includes() >= 0 && n.includes() <= 9)) {
+        
+        return 'invalid'
+    }else{
+
         return true ;
     }
+   }
+   
 
 }
 
-
-const obj =loanEligible({ age: 25, income: 30000 }) ;
-console.log(obj);
+const number = validMobile(["01712345678"]);
+console.log(number);
 
