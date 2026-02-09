d38f3dacd8e3736d526dc15dcd73f52d447cf238
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-02-09 13:46:22 +0600

    extra-problemsss.js

--- extra-Problem.js
+++ extra-Problem.js
@@ -1,192 +1,311 @@
-// // function finalScore(obj) {
+// // // // function finalScore(obj) {
     
-// //     const questions = obj.correct + obj.incorrect + obj.blank;
+// // // //     const questions = obj.correct + obj.incorrect + obj.blank;
 
-// //     if (questions !== 80 || typeof questions !== 'number') {
-// //         return 'invalid';
+// // // //     if (questions !== 80 || typeof questions !== 'number') {
+// // // //         return 'invalid';
+// // // //     }
+
+// // // //     const correctMark = obj.correct * 1.25 ;
+// // // //     const incorrectMark = obj.incorrect * 0.75 ;
+// // // //     const skipMark = obj.blank * 0 ;
+
+// // // //     const finalScore = (correctMark - incorrectMark ) ;
+// // // //     return Math.round(finalScore) ;
+
+    
+// // // // }
+
+// // // // const obj= { correct: 50, incorrect: 20, blank: 10 }
+
+// // // // const score = finalScore(obj);
+// // // // console.log(score);
+
+// // // // problem 1
+// // // function electricBill(unit, rate) {
+// // //    // your code here
+
+// // //    if (typeof unit !== 'number' || typeof rate !== 'number' || unit <= 0 || rate <=0) {
+// // //         return 'Invalid';
+// // //    }
+
+// // //    const totalBill = (unit * rate ).toFixed(2);
+// // //    return totalBill ;
+// // // }
+
+// // // const result = electricBill(50 , 2);
+// // // console.log(result);
+
+
+// // // // problem 2
+// // // function validUsername(username) {
+// // //    // your code here
+
+   
+   
+// // //    if (typeof username !== 'string') {
+// // //        return 'Invalid'
+// // //     }
+    
+// // //     const firstCharacter = username[0];
+    
+// // //     if (username.length < 5 || username.length > 15) {
+// // //         return false ;
+
+// // //     } else if (username.includes(' ') || username === '') {
+// // //         return false ;
+
+// // //     }else if (!((firstCharacter >= 'a' && firstCharacter <= 'z') ||(firstCharacter >= 'A' && firstCharacter <= 'Z') ) ) {
+// // //         return false;
+// // //     }else{
+// // //         return true ;
+// // //     }
+
+// // // }
+
+// // // const username = validUsername("ArSa1j");
+// // // console.log(username);
+
+
+// // // // PROBLEM 3
+// // // function loanEligible(info) {
+// // //    // your code here
+
+// // //    if (typeof info !== 'object') {
+// // //     return 'Invalid';
+
+// // //    }
+
+// // //    if ( !( info.hasOwnProperty('age') &&  info.hasOwnProperty('income')  && info.hasOwnProperty('hasJob')  )) {
+// // //         return 'invalid';
+// // //    }
+
+// // //     if (info.age < 21 || info.income <20000 || info.hasJob === false ) {
+        
+// // //         return false ;
+// // //     } else {
+// // //         return true ;
+// // //     }
+
+// // // }
+
+
+// // // const obj =loanEligible({ age: 25, income: 30000 }) ;
+// // // console.log(obj);
+
+
+
+// // // problem 1
+
+// // function groceryDiscount(price, discount) {
+// //    // your code here
+
+// //    if ( !(typeof price ==='number' && typeof discount ==='number')) {
+// //         return 'Invalid';
+
+// //     }else if (price < 0  ||  discount < 0 || discount > 100) {
+        
+// //         return 'Invalid';
 // //     }
 
-// //     const correctMark = obj.correct * 1.25 ;
-// //     const incorrectMark = obj.incorrect * 0.75 ;
-// //     const skipMark = obj.blank * 0 ;
+// //     const newPrice = price - (price * discount / 100 );
+// //     return newPrice.toFixed(2) ;
 
-// //     const finalScore = (correctMark - incorrectMark ) ;
-// //     return Math.round(finalScore) ;
-
-    
 // // }
 
-// // const obj= { correct: 50, incorrect: 20, blank: 10 }
-
-// // const score = finalScore(obj);
-// // console.log(score);
-
-// // problem 1
-// function electricBill(unit, rate) {
-//    // your code here
-
-//    if (typeof unit !== 'number' || typeof rate !== 'number' || unit <= 0 || rate <=0) {
-//         return 'Invalid';
-//    }
-
-//    const totalBill = (unit * rate ).toFixed(2);
-//    return totalBill ;
-// }
-
-// const result = electricBill(50 , 2);
-// console.log(result);
+// // const finalPrice = groceryDiscount(1000, 100);
+// // console.log(finalPrice);
 
 
-// // problem 2
-// function validUsername(username) {
-//    // your code here
 
-   
-   
-//    if (typeof username !== 'string') {
-//        return 'Invalid'
-//     }
-    
-//     const firstCharacter = username[0];
-    
-//     if (username.length < 5 || username.length > 15) {
-//         return false ;
+// // // problem 2
+// // function validMobile(mobile) {
+// //    // your code here
 
-//     } else if (username.includes(' ') || username === '') {
-//         return false ;
+// //    if (typeof mobile !== 'string') {
+// //         return 'Invalid';
+// //    }
 
-//     }else if (!((firstCharacter >= 'a' && firstCharacter <= 'z') ||(firstCharacter >= 'A' && firstCharacter <= 'Z') ) ) {
-//         return false;
-//     }else{
-//         return true ;
-//     }
+// //    if ( mobile.length !== 11) {
+// //         return false;
 
-// }
+// //    }else if (mobile.startsWith('01') !== true) {
+// //         return false ;
+// //    }
 
-// const username = validUsername("ArSa1j");
-// console.log(username);
+// //    const num = mobile.split('');
 
-
-// // PROBLEM 3
-// function loanEligible(info) {
-//    // your code here
-
-//    if (typeof info !== 'object') {
-//     return 'Invalid';
-
-//    }
-
-//    if ( !( info.hasOwnProperty('age') &&  info.hasOwnProperty('income')  && info.hasOwnProperty('hasJob')  )) {
-//         return 'invalid';
-//    }
-
-//     if (info.age < 21 || info.income <20000 || info.hasJob === false ) {
+// //    for(let n of num){
+// //     if (n < 0 || n > 9) {
         
-//         return false ;
-//     } else {
-//         return true ;
-//     }
-
-// }
-
-
-// const obj =loanEligible({ age: 25, income: 30000 }) ;
-// console.log(obj);
-
-
-
-// problem 1
-
-function groceryDiscount(price, discount) {
-   // your code here
-
-   if ( !(typeof price ==='number' && typeof discount ==='number')) {
-        return 'Invalid';
-
-    }else if (price < 0  ||  discount < 0 || discount > 100) {
-        
-        return 'Invalid';
-    }
-
-    const newPrice = price - (price * discount / 100 );
-    return newPrice.toFixed(2) ;
-
-}
-
-const finalPrice = groceryDiscount(1000, 100);
-console.log(finalPrice);
-
-
-
-// problem 2
-function validMobile(mobile) {
-   // your code here
-
-   if (typeof mobile !== 'string') {
-        return 'Invalid';
-   }
-
-   if ( mobile.length !== 11) {
-        return false;
-
-   }else if (mobile.startsWith('01') !== true) {
-        return false ;
-   }
-
-   const num = mobile.split('');
-
-   for(let n of num){
-    if (n < 0 || n > 9) {
-        
-        return false'
+// //         return false
     
-    }
+// //     }
    
 
-}
+// // }
 
-const number = validMobile(["01712345678"]);
-console.log(number);
-
-
+// // const number = validMobile(["01712345678"]);
+// // console.log(number);
 
 
-//problem 3
-function studentGrade(student) {
-   // your code here
+
+
+// // //problem 3
+// // function studentGrade(student) {
+// //    // your code here
    
-    if (!(student.hasOwnProperty('name') === true && student.hasOwnProperty('marks') === true )) {
-       return 'Invalid';
+// //     if (!(student.hasOwnProperty('name') === true && student.hasOwnProperty('marks') === true )) {
+// //        return 'Invalid';
 
-    }
+// //     }
     
-    if (student.marks < 0 || student.marks > 100) {
-        return 'Invalid';
+// //     if (student.marks < 0 || student.marks > 100) {
+// //         return 'Invalid';
  
-    }else if (student.marks >= 90 && student.marks <= 100 ) {
-        return 'A';
+// //     }else if (student.marks >= 90 && student.marks <= 100 ) {
+// //         return 'A';
 
-    } else if (student.marks >= 80 && student.marks <= 89 ) {
-        return 'B';
+// //     } else if (student.marks >= 80 && student.marks <= 89 ) {
+// //         return 'B';
         
-    } else if (student.marks >= 70 && student.marks <= 79 ) {
-        return 'C';
+// //     } else if (student.marks >= 70 && student.marks <= 79 ) {
+// //         return 'C';
         
-    } else if (student.marks >= 60 && student.marks <= 69 ) {
-        return 'D';
+// //     } else if (student.marks >= 60 && student.marks <= 69 ) {
+// //         return 'D';
         
-    } else if (student.marks < 60 ) {
-        return 'F';
+// //     } else if (student.marks < 60 ) {
+// //         return 'F';
         
+// //     } 
+
+
+// // }
+
+
+// // const grade = studentGrade("Raj");
+// // console.log(grade);
+
+
+
+// // problem 1 
+
+// function smartResult(arr) {
+//     // write your code here
+//     if (Array.isArray(arr) === false ) {
+        
+//         return 'Invalid';
+//     }
+
+//     let arrTotal = null ;
+//     for ( const a of arr){
+
+//         if (typeof a !== 'number' || a < 0 || a > 100) {
+            
+
+//             continue ;
+
+//         } else {
+
+//             arrTotal += a ;
+//         }
+
+//     }
+
+//     if (arrTotal === null) {
+//         return 'Invalid';
+//     }
+    
+
+//     const average = arrTotal / arr.length ;
+
+//     let status = null ;
+
+//     if (average >= 40) {
+//         status = 'Pass';
+
+//     } else{
+//         status =  'Fail' ;
+//     }
+    
+//     return{
+//         average: Math.round(average),
+//         status,
+//     }
+    
+
+// }
+
+
+// const arr = smartResult([45, 60, "70", -10, 90, 110, 30]);
+// console.log(arr);
+
+
+
+
+
+
+function expenseReport(data) {
+    // write your code here
+
+    if (Array.isArray(data) !== true) {
+        return 'Invalid';
+
     } 
 
 
+
+    let validExpense = [] ;
+    let totalAmount = 0
+    let amount = 0
+    let topCategory = null ;
+
+    for ( const arr of data) {
+
+        // console.log(typeof arr);
+        
+        if (typeof arr === 'object' && typeof arr.category === 'string' && typeof arr.amount === 'number' && arr.amount > 0) {
+            
+            validExpense.push(arr)
+            totalAmount += arr.amount ;
+
+            if ( arr.amount > amount) {
+                
+                topCategory = arr.category ;
+            }
+
+        } 
+    }
+
+    if (validExpense.length === 0) {
+        
+        return 'Invalid'
+    }
+    
+    
+    const average = Math.round(totalAmount / validExpense.length);
+    // console.log(validExpense , totalAmount , topCategory , average);
+
+    return {
+
+        total : totalAmount ,
+        average ,
+        topCategory
+    }
+
 }
 
 
-const grade = studentGrade("Raj");
-console.log(grade);
+const expense = expenseReport([
+  { category: "food", amount: 1200 },
+  { category: "rent", amount: 8000 },
+  { category: "food", amount: -300 },
+  { category: "travel", amount: 1500 },
+  { category: 123, amount: 500 },
+  { category: "rent", amount: 2000 }
+]);
 
 
-
+console.log(expense);
 
